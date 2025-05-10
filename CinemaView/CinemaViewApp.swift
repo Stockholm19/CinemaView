5c4272ffd1a30fd96cb3b21cb30833a37faf6f37
Abrikozs <Abrikozs@gmail.com>
2025-05-10 22:18:40 +0300

    Завершение приложения при закрытии окна на macOS

--- CinemaView/CinemaViewApp.swift
+++ CinemaView/CinemaViewApp.swift
@@ -5,10 +5,19 @@
 //  Created by Роман Пшеничников on 10.05.2025.
 //
 
+import Cocoa
 import SwiftUI
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+}
 
 @main
 struct CinemaViewApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             if let url = URL(string: "https://hdrezka.co/") {
