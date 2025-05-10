977bdd85faec1f1b79584366339fdc27cc8d33a3
Abrikozs <Abrikozs@gmail.com>
2025-05-10 21:28:18 +0300

    Initial Commit

---
+++ CinemaView/CinemaViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaViewApp.swift
+//  CinemaView
+//
+//  Created by Роман Пшеничников on 10.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CinemaViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
